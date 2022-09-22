e81cb88c6bd58bd95503027e28b53f4bb86cdd76
73X73-1LL1518L3 <107544841+73X73-1LL1518L3@users.noreply.github.com>
2022-09-22 14:53:22 +0200

    fisrt save

---
+++ script.js
@@ -0,0 +1,211 @@
+/*
+const item1 = {
+    image: "https://i.ytimg.com/vi/YHLTIQAi9Lw/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAVG3g7D7Q4LfTGV5t4DHZXWvLzEg",
+    title: "Tourist trophy - suicidal race dash cam",
+    logoChain: "https://yt3.ggpht.com/EGNOjeMsf7ab_Mwne-kDrIRO5_4AzVBC-gG5h2qD6916rBbA9d3ih61c-gZGjswwmNj5LxWQHw=s176-c-k-c0x00ffffff-no-rj-mo",
+    chainName: "tythanoss2",
+    statView: "il y a 3 jours 13 minutes et 2 secondes - 1 387 vues",
+};
+
+const item2 = {
+    image: "https://i.ytimg.com/vi/FruIIkl241I/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAh-qVtvYOEVUXykGQKoXiha5Iifg",
+    title: "La moto la plus déraisonnable sur route - Bimota Tesi H2 - ESSAI",
+    logoChain: "https://yt3.ggpht.com/ytc/AMLnZu8Moen923cHNmbhWMx7IVGt1mtYwOF3piPjuqjD=s68-c-k-c0x00ffffff-no-rj",
+    chainName: "High Side",
+    statView: "il y a 6 jours 9 minutes et 29 secondes - 369 825 vues"
+};
+
+const item3 = {
+    image: "https://i.ytimg.com/vi/bh1R_6vWgLQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC4tBfx10jSrAzmKZ1_w_yBfUI2AA",
+    title: "Tuto : Apprendre le wheeling à moto ?",
+    logoChain: "https://yt3.ggpht.com/ytc/AMLnZu-RittBvLepQGajozq9z6YXtQzqOXbYSxV5HYdXgQ=s68-c-k-c0x00ffffff-no-rj",
+    chainName: "Lord Puma",
+    statView: "il y a 6 ans 14 minutes et 18 - secondes 869 875 vues",
+};
+
+const item4 = {
+    image: "https://i.ytimg.com/vi/ugZKd4mqvwQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCpN7s3mKTGr6wov3Rp_khfXqlryw",
+    title: "HP4 Race Isle of Man",
+    logoChain: "https://yt3.ggpht.com/ytc/AMLnZu9DmSFRMqS0-vlTUF20PIngdvbZg9IZl3Xa28OH=s68-c-k-c0x00ffffff-no-rj",
+    chainName: "BMW Motorrad UK",
+    statView: "il y a 5 ans 19 minutes - 5 529 201 vues",
+};
+
+const item5 = {
+    image: "https://i.ytimg.com/vi/9MfWva3yj-0/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA4J5VjS36pHQgGgoy2AEYulUd6PA",
+    title: "2022 Motorcycle Racing Crash Compilation #1",
+    logoChain: "https://yt3.ggpht.com/fpeP4PDgCiBmhB91CY572Qfsrs8d4L2_kpfeNFOb_diUnfungfZG2sS2BrAtwYxNEdRZlNDIxvs=s68-c-k-c0x00ffffff-no-rj",
+    chainName: "Motorsport TV",
+    statView: "il y a 2 mois 14 minutes et 59 secondes - 2 177 988 vues",
+};
+
+const item6 = {
+    image: "https://i.ytimg.com/vi/vpPfM8Ob7Wo/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBFEbdcqd2oF6Fy8iWyF7iWW2rvnw",
+    title: "Hypertest 2022 : Kawasaki Ninja ZX-10R",
+    logoChain: "https://yt3.ggpht.com/ytc/AMLnZu9ifumuFhPeJdvBB5m-dw1pzANBISz0SrbpOyMs=s68-c-k-c0x00ffffff-no-rj",
+    chainName: "Moto et Motards",
+    statView: "il y a 7 jours 6 minutes et 50 secondes - 52 912 vues",
+};
+
+const item7 = {
+    image: "https://i.ytimg.com/vi/LwZzpOSF8qQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC6m1PHmQlrZB-uIel8QsSaY6sGGQ",
+    title: "Essai Triumph Rocket 3 R et GT",
+    logoChain: "https://yt3.ggpht.com/ytc/AMLnZu-uRSZtEGVaFvfais1U0qcxetQ9UpfQY8aBwtyDxw=s68-c-k-c0x00ffffff-no-rj",
+    chainName: "Le Repaire des Motards",
+    statView: "il y a 2 ans 13 minutes et 32 secondes - 148 251 vues",
+};
+
+const item8 = {
+    image: "https://i.ytimg.com/vi/yEpnTD3U4eI/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCb39ODm1Cow6VW3wQhOSKLOF1_mg",
+    title: "GoPro Hero 11 Black On A Yamaha R6",
+    logoChain: "https://yt3.ggpht.com/ytc/AMLnZu8wPZl8hQUd1lIXoLwh10JLxZWmrjiWv_4Y-2_I=s68-c-k-c0x00ffffff-no-rj",
+    chainName: "SuperBike Racer",
+    statView: "il y a 3 jours 6 minutes et 46 secondes - 38 537 vues",
+};
+*/
+
+const item1 = {
+    image: "https://i.ytimg.com/vi/YHLTIQAi9Lw/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAVG3g7D7Q4LfTGV5t4DHZXWvLzEg",
+    title: "Tourist trophy - suicidal race dash cam",
+    logoChain: "https://yt3.ggpht.com/EGNOjeMsf7ab_Mwne-kDrIRO5_4AzVBC-gG5h2qD6916rBbA9d3ih61c-gZGjswwmNj5LxWQHw=s176-c-k-c0x00ffffff-no-rj-mo",
+    chainName: "tythanoss2",
+    statView: "il y a 3 jours 13 minutes et 2 secondes - 1 387 vues",
+};
+
+const item2 = {
+    image: "https://i.ytimg.com/vi/FruIIkl241I/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAh-qVtvYOEVUXykGQKoXiha5Iifg",
+    title: "La moto la plus déraisonnable sur route - Bimota Tesi H2 - ESSAI",
+    logoChain: "https://yt3.ggpht.com/ytc/AMLnZu8Moen923cHNmbhWMx7IVGt1mtYwOF3piPjuqjD=s68-c-k-c0x00ffffff-no-rj",
+    chainName: "High Side",
+    statView: "il y a 6 jours 9 minutes et 29 secondes - 369 825 vues"
+};
+
+const item3 = {
+    image: "https://i.ytimg.com/vi/bh1R_6vWgLQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC4tBfx10jSrAzmKZ1_w_yBfUI2AA",
+    title: "Tuto : Apprendre le wheeling à moto ?",
+    logoChain: "https://yt3.ggpht.com/ytc/AMLnZu-RittBvLepQGajozq9z6YXtQzqOXbYSxV5HYdXgQ=s68-c-k-c0x00ffffff-no-rj",
+    chainName: "Lord Puma",
+    statView: "il y a 6 ans 14 minutes et 18 - secondes 869 875 vues",
+};
+
+const item4 = {
+    image: "https://i.ytimg.com/vi/ugZKd4mqvwQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCpN7s3mKTGr6wov3Rp_khfXqlryw",
+    title: "HP4 Race Isle of Man",
+    logoChain: "https://yt3.ggpht.com/ytc/AMLnZu9DmSFRMqS0-vlTUF20PIngdvbZg9IZl3Xa28OH=s68-c-k-c0x00ffffff-no-rj",
+    chainName: "BMW Motorrad UK",
+    statView: "il y a 5 ans 19 minutes - 5 529 201 vues",
+};
+
+const item5 = {
+    image: "https://i.ytimg.com/vi/9MfWva3yj-0/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA4J5VjS36pHQgGgoy2AEYulUd6PA",
+    title: "2022 Motorcycle Racing Crash Compilation #1",
+    logoChain: "https://yt3.ggpht.com/fpeP4PDgCiBmhB91CY572Qfsrs8d4L2_kpfeNFOb_diUnfungfZG2sS2BrAtwYxNEdRZlNDIxvs=s68-c-k-c0x00ffffff-no-rj",
+    chainName: "Motorsport TV",
+    statView: "il y a 2 mois 14 minutes et 59 secondes - 2 177 988 vues",
+};
+
+const item6 = {
+    image: "https://i.ytimg.com/vi/vpPfM8Ob7Wo/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBFEbdcqd2oF6Fy8iWyF7iWW2rvnw",
+    title: "Hypertest 2022 : Kawasaki Ninja ZX-10R",
+    logoChain: "https://yt3.ggpht.com/ytc/AMLnZu9ifumuFhPeJdvBB5m-dw1pzANBISz0SrbpOyMs=s68-c-k-c0x00ffffff-no-rj",
+    chainName: "Moto et Motards",
+    statView: "il y a 7 jours 6 minutes et 50 secondes - 52 912 vues",
+};
+
+const item7 = {
+    image: "https://i.ytimg.com/vi/LwZzpOSF8qQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC6m1PHmQlrZB-uIel8QsSaY6sGGQ",
+    title: "Essai Triumph Rocket 3 R et GT",
+    logoChain: "https://yt3.ggpht.com/ytc/AMLnZu-uRSZtEGVaFvfais1U0qcxetQ9UpfQY8aBwtyDxw=s68-c-k-c0x00ffffff-no-rj",
+    chainName: "Le Repaire des Motards",
+    statView: "il y a 2 ans 13 minutes et 32 secondes - 148 251 vues",
+};
+
+const item8 = {
+    image: "https://i.ytimg.com/vi/yEpnTD3U4eI/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCb39ODm1Cow6VW3wQhOSKLOF1_mg",
+    title: "GoPro Hero 11 Black On A Yamaha R6",
+    logoChain: "https://yt3.ggpht.com/ytc/AMLnZu8wPZl8hQUd1lIXoLwh10JLxZWmrjiWv_4Y-2_I=s68-c-k-c0x00ffffff-no-rj",
+    chainName: "SuperBike Racer",
+    statView: "il y a 3 jours 6 minutes et 46 secondes - 38 537 vues",
+};
+
+const arrayItem =[
+    item1,
+    item2,
+    item3,
+    item4,
+    item5,
+    item6,
+    item7,
+    item8,
+];
+
+
+
+const youtubeSection = document.getElementById("youtube-section");
+
+//Créer un bloucle pour afficher les 8 images différentes
+function chargeAllImage() {
+    for(let i = 0; i <arrayItem.length; i++){
+        youtubeSection.innerHTML +=
+        `
+        <section>
+            <div class="item-number">
+                <img class="image-item" src="${arrayItem[i].image}">
+                <div class="container-logo-title">
+                    <img class="logo-chain" src="${arrayItem[i].logoChain}">
+                    <p class="title">${arrayItem[i].title}</p>
+                </div>
+                <div class="container-chain-stat">
+                    <p class="chain-name">${arrayItem[i].chainName}</p>
+                    <p class="stat-view">${arrayItem[i].statView}</p>
+                </div>
+            </div>
+        </section>
+        `
+    };
+}
+chargeAllImage()
+
+
+
+//Créer une barre de rechercher pour filtrer les vidéos
+const searchInput = document.querySelector('#input')
+
+searchInput.addEventListener("input", (e) => {
+    let element = e.target.value.toLowerCase();
+    const filteredArrayItem = arrayItem.filter(array => array.title.toLocaleLowerCase().includes(element))
+    console.log(element)
+})
+
+for(let i = 0; i < filteredArrayItem.length; i++){
+    
+    arrayItem.filter(title),
+
+    arrayItem.innerHTML +=
+    `
+    <section>
+        <div class="item-number">
+            <img class="image-item" src="${arrayItem[i].image}">
+            <div class="container-logo-title">
+                <img class="logo-chain" src="${arrayItem[i].logoChain}">
+                <p class="title">${arrayItem[i].title}</p>
+            </div>
+            <div class="container-chain-stat">
+                <p class="chain-name">${arrayItem[i].chainName}</p>
+                <p class="stat-view">${arrayItem[i].statView}</p>
+            </div>
+        </div>
+    </section>
+    `
+}
+
+
+search()
+
+
+
+
+
+
+
+
